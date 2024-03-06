d9f9c8cc4d703270d5b74e16a7cb5a53d277826f
Rafia Ashraf <rafiaashraf122@gmail.com>
2024-03-06 23:13:10 +0500

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome");
+});
+
+const PORT = 5000;
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
